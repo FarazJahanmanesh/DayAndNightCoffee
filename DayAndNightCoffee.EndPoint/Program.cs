95561b505eda2cc41d8b1f8dec9fa676f45d1cdd
faraz jahan manesh <farazebipax@gmail.com>
2023-02-23 13:53:24 -0800

    add the dependency injection to project

--- DayAndNightCoffee.EndPoint/Program.cs
+++ DayAndNightCoffee.EndPoint/Program.cs
@@ -1,7 +1,14 @@
+using DayAndNightCoffee.Core.Application.Services;
+using DayAndNightCoffee.Core.Domain.Contracts;
 using DayAndNightCoffee.Infra.Database;
+using DayAndNightCoffee.Infra.Database.Repository;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddScoped<IProductServices, ProductServices>();
+
 builder.Services.AddDbContext<DayAndNightCoffeeDbContext>(options =>
     options.UseSqlServer("Server=.;Database=DayAndNightCoffee;Trusted_Connection=True;"));
 var app = builder.Build();
