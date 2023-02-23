455b3768eb277699ee96f8e645903ba5ce4a7b97
faraz jahan manesh <farazebipax@gmail.com>
2023-02-23 12:42:04 -0800

    make database and a table in sql

---
+++ DayAndNight.Infra.Database/DayAndNightCoffeeDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using System.Text;
+using System.Threading.Tasks;
+using DayAndNightCoffee.Core.Domain.Entities;
+
+namespace DayAndNightCoffee.Infra.Database
+{
+    public class DayAndNightCoffeeDbContext:DbContext
+    {
+        public DayAndNightCoffeeDbContext(DbContextOptions<DayAndNightCoffeeDbContext> options) :base(options)
+        {
+
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
--- DayAndNightCoffee.EndPoint/Program.cs
+++ DayAndNightCoffee.EndPoint/Program.cs
@@ -1,4 +1,9 @@
+using DayAndNightCoffee.Infra.Database;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<DayAndNightCoffeeDbContext>(options =>
+    options.UseSqlServer("Server=.;Database=DayAndNightCoffee;Trusted_Connection=True;"));
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
