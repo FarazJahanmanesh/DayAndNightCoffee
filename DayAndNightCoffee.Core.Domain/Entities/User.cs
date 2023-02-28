192f0ecb7e72426e14cb47bd7b1d16b3e9b6739d
faraz jahan manesh <farazebipax@gmail.com>
2023-02-27 16:28:32 -0800

    add new entities

---
+++ DayAndNightCoffee.Core.Domain/Entities/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DayAndNightCoffee.Core.Domain.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string phone { get; set; }
+        public string Password { get; set; }
+        public Cart cart { get; set; }
+        public DateTime Created { get; set; }= DateTime.Now;
+    }
+}
